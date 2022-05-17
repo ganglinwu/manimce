d92e2d958166f463309d92885b784831338120cc
Pentagon Learning <ganglin.wu@gmail.com>
2022-05-17 17:04:06 +0800

    added polygon function, to be completed later on

--- 02 Work in Progress/03 internal angle of polygon/scene.py
+++ 02 Work in Progress/03 internal angle of polygon/scene.py
@@ -1,3 +1,4 @@
+import math
 from manim import *
 
 class polygonintangle(MovingCameraScene):
@@ -14,4 +15,18 @@ class polygonintangle(MovingCameraScene):
         for i in range (4,n+1):
             
 
+#a function that draws polygons
+def polygon(n):
+    ref_ang = 2*PI/n
+    #it seems like the regular polygons with
+    #even number of sides have a "flat"
+    #top while those with odd numbers 
+    #have a pointy top
+    #so we are going to offset the first vertex
+    #by half the reference angle
+    #if the number of sides is even
+    if n%2 ==0:
+        first_vertex_ang = (PI/2 - 0.5*ref_ang)
+    else:
+        first_vertex_ang = PI/2
 
